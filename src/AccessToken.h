ecfadbd7bbbf5ab70c29b2a6bbd50acce1c6bb7e
cool <niu199212cool@163.com>
2017-08-30 10:53:16 +0800

    test

--- src/AccessToken.h
+++ src/AccessToken.h
@@ -7,4 +7,5 @@
 static size_t callback(void *ptr, size_t size, size_t nmemb, void *stream);
 int get_access_token(std::string &access_token, const std::string &AK, const std::string &SK);
 
+
 #endif
