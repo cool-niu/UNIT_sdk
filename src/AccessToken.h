4a485e5d848f81817af0fb532200a62a4e542979
cool <niu199212cool@163.com>
2017-08-30 09:34:20 +0800

    “modify”

--- src/AccessToken.h
+++ src/AccessToken.h
@@ -3,6 +3,7 @@
 
 #include <string>
 
+
 static size_t callback(void *ptr, size_t size, size_t nmemb, void *stream);
 int get_access_token(std::string &access_token, const std::string &AK, const std::string &SK);
 
